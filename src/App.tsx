6a2ef850556c50468cb5c8927c0bc6f6655c31a0
Jacob Seiler <seiler.jacob1@gmail.com>
2024-03-29 00:07:13 -0400

    Added decks

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
-  const [count, setCount] = useState(0);
+interface Deck {
+  id: string;
+  title: string;
+  colour: string;
+}
+
+const generateHex = () => {
+  // 20 options for saturation
+  const cells = 20;
+  const h = Math.floor(Math.random() * (360 / cells + 1)) * cells;
+
+  // Fixed saturation and lightness
+  const s = 65;
+  const l = 60 / 100;
+
+  // Convert HSL to HEX
+  const a = (s * Math.min(l, 1 - l)) / 100;
+  const f = (n: number) => {
+    const k = (n + h / 30) % 12;
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return Math.round(255 * color)
+      .toString(16)
+      .padStart(2, "0"); // convert to Hex and prefix "0" if needed
+  };
+
+  return `#${f(0)}${f(8)}${f(4)}`;
+};
+
+const App = () => {
+  const [decks, setDecks] = useState<Deck[]>([]);
+
+  // Load decks from local storage
+  useEffect(() => {
+    setDecks(JSON.parse(localStorage.getItem("decks") ?? JSON.stringify([])));
+  }, []);
+
+  const handleAdd = () => {
+    const newDeck = {
+      id: uuidv4(),
+      title: "New Deck",
+      colour: generateHex(),
+    };
+
+    setDecks((prev) => {
+      const newDecks = [...prev, newDeck];
+      localStorage.setItem("decks", JSON.stringify(newDecks));
+
+      return newDecks;
+    });
+  };
 
   return (
-    <>
-      <h1 className="font-bold">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <h1>Welcome section!</h1>
+      <hr />
+      {!decks ? (
+        <p>You don&apos;t have any decks!</p>
+      ) : (
+        decks.map((deck) => (
+          <div
+            key={deck.id}
+            className="w-60 h-32"
+            style={{ backgroundColor: deck.colour }}
+          >
+            <p>{deck.title}</p>
+          </div>
+        ))
+      )}
+      <button onClick={handleAdd}>Add Deck</button>
+    </main>
   );
-}
+};
 
 export default App;
